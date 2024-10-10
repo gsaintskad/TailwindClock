9e67a1079a2f5cb43b28ef5e0b22503e8bd138a0
Hlib Bezuhlyi <30008@s.pm.szczecin.pl>
2024-10-10 22:18:48 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-
-
 function App() {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
-      <h1 className='text-green-50'>MVP</h1>
-  
+      <h1 className="text-green-500">MVP</h1>
     </div>
   );
 }
