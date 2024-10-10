2f5821b82f271ee7406238e8c3715970441f7bc9
Hlib Bezuhlyi <30008@s.pm.szczecin.pl>
2024-10-11 00:03:17 +0200

    ClockArrows were implemented :)

--- src/Components/Clock.tsx
+++ src/Components/Clock.tsx
@@ -1,15 +1,27 @@
 interface hourMarksProps {
   angle?: number;
+  rotationPeriod?: number;
 }
 
-const HourMarks = (props: hourMarksProps) => {
-  return <div className={`flex w-3 flex-col h-1/2 bg-amber-200 ${props.angle!>=0?'':'-'}rotate-${props.angle!}`}></div>;
+const ClockArrow = (props: hourMarksProps) => {
+  return (
+    <div
+      style={
+        {
+          "--tw-rotate-start": `${props.angle || 0}deg`,
+          "--tw-rotate-period": `${props.rotationPeriod || 60}s`,
+        } as React.CSSProperties
+      }
+      className="absolute left-1/2  flex w-3 flex-col lef h-1/2 bg-amber-200 transform origin-bottom animate-spin-variable"
+    ></div>
+  );
 };
-//relative top-1/2 left-1/2
+
 const Clock = () => {
   return (
-    <div className="w-1/2 aspect-square bg-blue-400 rounded-full">
-     <HourMarks angle={150}/>
+    <div className="relative w-1/2 aspect-square bg-blue-400 rounded-full">
+      <ClockArrow angle={30} rotationPeriod={15} />
+      <ClockArrow angle={10} />
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{html,js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      rotate:(()=>{
-        const rotations={};
-        for(let i=0;i<=360;i++){
-          rotations[i]=`${i}deg`;
-        }
-        return rotations;
-      })()
+      // rotate:(()=>{
+      //   const rotations={};
+      //   for(let i=0;i<=360;i++){
+      //     rotations[i]=`${i}deg`;
+      //   }
+      //   return rotations;
+      // })()
+      animation: {
+        "spin-variable":
+          "spin-variable var(--tw-rotate-period, 60s) linear infinite",
+      },
+      keyframes: {
+        "spin-variable": {
+          "0%": { transform: "rotate(var(--tw-rotate-start, 0deg))" },
+          "100%": {
+            transform: "rotate(calc(360deg + var(--tw-rotate-start, 0deg)))",
+          },
+        },
+      },
     },
   },
   plugins: [],
-}
-
+};
